6dbe6851e75a15da20318e991aeb0983447d1465
Suresh Melvin Sigera <sureshmelvinsigera@users.noreply.github.com>
2019-06-07 19:01:02 -0400

    Example ReactDOM.render()

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const heading = React.createElement(
+    "h1",
+    {
+        id: 'page-header',
+        className: 'heading',
+    },
+    "This is heading"
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+    heading,
+    document.getElementById("root")
+);
